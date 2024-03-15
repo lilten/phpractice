a75075d73a06beb08d526687bb66fb481c3bd61a
lilten <125046502+lilten@users.noreply.github.com>
2024-03-15 13:00:39 +0600

    Add files via upload

---
+++ lesson3.php
@@ -0,0 +1,14 @@
+<?php
+
+$greeting = "Hello";
+$name = "Anna";
+echo "{$greeting}, {$name}<br>";
+
+$firstLine = "\"Аптеку позабудь ты для венков лавровых<br>";
+$secondLine = "И не мори больных, но усыпляй здоровых\"<br>";
+echo "{$firstLine} {$secondLine} <br>";
+
+$secondsInMinute = 60;
+$minutesInHour = 60;
+$result = $secondsInMinute * $minutesInHour;
+echo "В часе {$result} секунд. <br>";
